a2829b80d57ea860df6914b7acca9060c4b5d96d
Spencer <sthompson.dev@outlook.com>
2023-11-09 23:48:17 -0700

    Experimenting again

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -9,8 +9,19 @@ from dotenv import load_dotenv
 class GPT():
 
     def __init__(self):
-        
         load_dotenv()
-        client = OpenAI(
+        self.client = OpenAI(
+            api_key=os.getenv('OPENAI_API_KEY'),
             organization=os.getenv('OPENAI_ORG_ID')
         )
+
+    def run(self):
+        completion = self.client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+            ]
+        )
+
+        return completion.choices[0].message
