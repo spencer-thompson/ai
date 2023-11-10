01ddfc52b3541b10979ccf4704a7b1434c7e33e7
Spencer Thompson <sthompson.dev@outlook.com>
2023-11-10 15:27:53 -0700

    added streaming responses

--- gpt/chatgpt.py
+++ gpt/chatgpt.py
@@ -5,7 +5,7 @@ load_dotenv()
 client = OpenAI()
 
 messages = [
-    {"role": "system", "content": "You are a chicken, you only respond with chicken noises"}
+    {"role": "system", "content": "Explain api endpoints like the user is 5"}
 ]
 
 user_input = input('User: ')
@@ -15,12 +15,20 @@ while user_input != 'q':
 
     completion = client.chat.completions.create(
         model = "gpt-4-1106-preview",
-        messages = messages
+        messages = messages,
+        stream = True
     )
 
-    messages.append(completion.choices[0].message)
+    total_response = ''
+    print("Assistant: ", end='')
+    for chunk in completion:
+        print(chunk.choices[0].delta.content, end='')
+        if chunk.choices[0].delta.content is not None:
+            total_response += chunk.choices[0].delta.content
+
+    messages.append({"role": "user", "content": total_response})
 
     print()
-    print(f'Assistant: {completion.choices[0].message.content}')
     print()
     user_input = input('User: ')
+    print()
