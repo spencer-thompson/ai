7b59fcdf0eeeb5b79e35387430313599fd89bb24
Spencer <sthompson.dev@outlook.com>
2023-11-10 21:13:07 -0700

    added simple docstrings

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -9,7 +9,13 @@ from dotenv import load_dotenv
 
 class GPT():
 
-    def __init__(self):
+    def __init__(
+            self,
+            sys_msg: str = "You are a helpful assistant.",
+            messages: list = [],
+            model: str = "gpt-4-1106-preview",
+
+    ):
 
         load_dotenv()
         self.client = OpenAI(
@@ -29,29 +35,33 @@ class GPT():
         return completion.choices[0].message
     
     def models(self):
+        """Returns all OpenAI models"""
         return self.client.models.list()
     
     # --- Want to build out each api endpoint ---
 
     # Chat
     def chat(self):
+        """Chat conversation"""
         pass
 
     def chats(self):
+        """Streaming Chat conversation"""
         pass
 
-    # TTS
     def speech(self):
+        """TTS"""
         pass
 
-    # STT
     def whisper(self):
+        """STT"""
         pass
 
     # Image gen
 
     # Moderation
     def mod(self, message: str):
+        """Moderation, pass in a string to get moderation scores."""
         return self.client.moderations.create(input=message)
 
     # Assistants
