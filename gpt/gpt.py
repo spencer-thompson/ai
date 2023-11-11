a342d9d16e8447b3b8c05311c7b7f048c61fc2d5
Spencer <sthompson.dev@outlook.com>
2023-11-10 21:37:06 -0700

    added a bunch of methods (untested)

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -14,25 +14,25 @@ class GPT():
             sys_msg: str = "You are a helpful assistant.",
             messages: list = [],
             model: str = "gpt-4-1106-preview",
+            temp: float = 0.7,
 
     ):
+        """Constructor"""
+        self.sys_msg = [
+            {"role": "system", "content": sys_msg}
+        ]
+        self.messages = messages
+        self.model = model
+        self.temp = temp
 
         load_dotenv()
         self.client = OpenAI(
             api_key=os.getenv("OPENAI_KEY"),
         )
 
-    # Testing method
-    def run(self):
-        completion = self.client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Hello!"}
-            ]
-        )
-
-        return completion.choices[0].message
+    def __str__(self):
+        """String representation of the AI"""
+        return f"Model: {self.model} | Temperature: {self.temp}"
     
     def models(self):
         """Returns all OpenAI models"""
@@ -41,13 +41,45 @@ class GPT():
     # --- Want to build out each api endpoint ---
 
     # Chat
-    def chat(self):
+    def chat(self, query: str):
         """Chat conversation"""
-        pass
 
-    def chats(self):
+        self.add_msg(role = "user", content = query)
+
+        completion = self.client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+            ]
+        )
+
+        self.add_msg(
+            role = completion.choices[0].message.role,
+            content = completion.choices[0].message.content
+        )
+
+        return completion.choices[0].message.content
+
+    def chats(self, query: str):
         """Streaming Chat conversation"""
-        pass
+
+        self.add_msg(role = "user", content = query)
+
+        completion = self.client.completions.create(
+            model = self.model,
+            messages = self.sys_msg + self.messages,
+            temperature = self.temp,
+            stream = True
+        )
+
+        total_response = ""
+        for chunk in completion:
+            if chunk.choices[0].delta.content is not None: # Will change for function calling
+                yield chunk.choices[0].delta.content
+                total_response += chunk.choices[0].delta.content
+
+        self.add_msg(role = "assistant", content = total_response)
 
     def speech(self):
         """TTS"""
@@ -65,6 +97,12 @@ class GPT():
         return self.client.moderations.create(input=message)
 
     # Assistants
+
+    # --- Messages Control ---
+
+    def add_msg(self, role: str, content: str):
+        """Adds a new message to context"""
+        self.messages.append({"role": role, "content": content})
     
 
 if __name__ == "__main__":
